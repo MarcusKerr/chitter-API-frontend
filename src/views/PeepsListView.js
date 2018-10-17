49029326a13672332cf4ed7374f4f541e784bc3a
Marcus <marcus.kerr94@gmail.com>
2018-10-17 19:47:33 +0100

    Refactored peeplistview create method, added format peep method

--- src/views/PeepsListView.js
+++ src/views/PeepsListView.js
@@ -9,23 +9,26 @@
     this._peepsList.getPeeps()
     .then(peepJson => {
       peepJson.forEach((peep) => {
-        this._peepsList_html.push(`
-        <div class="peep-container container pt-2 pb-2">
-          <div class="peep-hearder">
-            <strong class="handle mr-1">@${peep.user.handle}</strong>
-            <span class="divider mr-1">.</span>
-            <small class="time">${peep.updated_at}</small>
-          </div>
-          <div class="peep-body">
-            <p class="peep-text">${peep.body}</p>
-          </div>
-          <div class="peep-footer">
-            <span class="likes">${peep.likes.length}</span>
-          </div>
-        </div>`);
+        this._peepsList_html.push(this._formatPeep(peep));
       });
-      return this._peepsList_html;
+      return `<div id="peep-list" class="col-md-6 mx-auto">${this._peepsList_html}<div>`;
     });
+  };
+
+  PeepsListView.prototype._formatPeep = function (peep) {
+    return `<div class="peep-container container pt-2 pb-2">
+              <div class="peep-hearder">
+                <strong class="handle mr-1">@${peep.user.handle}</strong>
+                <span class="divider mr-1">.</span>
+                <small class="time">${peep.updated_at}</small>
+              </div>
+              <div class="peep-body">
+                <p class="peep-text">${peep.body}</p>
+              </div>
+              <div class="peep-footer">
+                <span class="likes">${peep.likes.length}</span>
+              </div>
+            </div>`
   };
 
   exports.PeepsListView = PeepsListView;
