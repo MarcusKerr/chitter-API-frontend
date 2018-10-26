17ffd613e595f8078e96956fb3884403343d4f9f
Marcus <marcus.kerr94@gmail.com>
2018-10-26 13:12:39 +0100

    client class  UT

--- spec/models/ClientSpec.js
+++ spec/models/ClientSpec.js
@@ -1,10 +1,20 @@
 'use strict'
 describe('Client', function() {
   var Client = require('../../src/models/Client').Client;
+  var url = "https://thisisaurl.com"
   var client;
+  var fetch;
 
   beforeEach(function() {
     client = new Client();
+    fetch = jasmine.createSpy('fetch');
+  });
+
+  describe(".connect", function() {
+    it("Should call fetch with url argument", function() {
+      client.connect(url);
+      expect(fetch).toHaveBeenCalledWith('url');
+    });
   });
 
 });
