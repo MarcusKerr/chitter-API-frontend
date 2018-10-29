f2880ececfd7c126f66d9197ef329bf7a7230090
Marcus <marcus.kerr94@gmail.com>
2018-10-29 16:44:48 +0000

    create Sign up View

---
+++ src/views/SignUpView.js
@@ -0,0 +1,11 @@
+(function(exports) {
+  function SignUpView () {
+
+  }
+
+  SignUpView.prototype.create = function () {
+    return `<div><h1>Sign Up</h1></div>`;
+  };
+
+  exports.SignUpView = SignUpView;
+})(this);
