af43beee8063b48d925950c3a4d33be4df6187c3
Marcus <marcus.kerr94@gmail.com>
2018-10-30 15:44:49 +0000

    Feature Test: Display homepage - RED

---
+++ spec/features/HomepageFeature.js
@@ -0,0 +1,9 @@
+var webdriverio = require('webdriverio');
+var expect = require('chai').expect;
+
+describe('Homepage Tests', function() {
+
+  it('displays homepage title', function(done) {
+
+  });
+});
