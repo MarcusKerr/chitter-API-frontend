7342b538f6e3e6eb4e80d9c9d2d5946f74d27c70
Marcus <marcus.kerr94@gmail.com>
2018-11-08 21:16:15 +0000

    UT: main controller, render login, delegates to logn view - RED

--- spec/controllers/MainControllerSpec.js
+++ spec/controllers/MainControllerSpec.js
@@ -6,15 +6,17 @@ describe("MainController", function() {
   var mainController;
   var indexView;
   var signUpView;
+  var logInView;
   var errorMessageView;
   var usersController;
 
   beforeEach(function() {
     indexView = jasmine.createSpyObj('indexView', ['create']);
     signUpView = jasmine.createSpyObj('signUpView', ['create']);
+    logInView = jasmine.createSpyObj('logInView', ['create']);
     errorMessageView = jasmine.createSpyObj('errorMessageView', ['create']);
     usersController = jasmine.createSpyObj('usersController', ['createNewUser']);
-    mainController = new MainController(indexView, signUpView, errorMessageView, usersController);
+    mainController = new MainController(indexView, signUpView, logInView, errorMessageView, usersController);
   })
 
   describe(".renderIndex", function() {
@@ -28,6 +30,13 @@ describe("MainController", function() {
     it("delagates to sign up view", function() {
       mainController.renderSignUp();
       expect(signUpView.create).toHaveBeenCalled();
+    });
+  });
+
+  describe(".renderLogIn", function() {
+    it("delegates to log in view", function() {
+      mainController.renderLogIn();
+      expect(logInView.create).toHavebeenCalled();
     });
   });
 
