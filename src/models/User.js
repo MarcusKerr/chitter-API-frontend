aaf5047449e2bad2c8430f519f2b3e172d2abf89
Marcus <marcus.kerr94@gmail.com>
2018-12-08 15:52:35 +0000

    refactor user

--- src/models/User.js
+++ src/models/User.js
@@ -4,11 +4,21 @@
   };
 
   User.prototype.new = function(handle, password) {
-    return this._client.post('/users', { user: { 'handle': handle, 'password': password } });
+    return this._client.post('/users', {
+      user: {
+        handle,
+        password
+      }
+    });
   };
 
   User.prototype.login = function(handle, password) {
-    return this._client.post('/sessions', { session: { 'handle': handle, 'password': password } });
+    return this._client.post('/sessions', {
+      session: {
+        handle,
+        password
+      }
+    });
   };
 
   exports.User = User;
