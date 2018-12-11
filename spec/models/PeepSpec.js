11bed5edd87109c07d635bf7bdc241280d49689e
Marcus <marcus.kerr94@gmail.com>
2018-12-11 13:27:01 +0000

    UT: peepSpec, peep.new, delegates to client - RED

---
+++ spec/models/PeepSpec.js
@@ -0,0 +1,20 @@
+'use strict';
+describe("Peep", function() {
+  const Peep = require("../../src/models/Peep").Peep;
+  const PeepData = require('../helpers/PeepData.json');
+  const mockSession = require('../helpers/mockSession.json');
+  var peep;
+  var mockClient;
+
+  beforeEach(function() {
+    mockClient = jasmine.createSpyObj('mockClient', ['post']);
+    peep = new Peep(mockClient);
+  });
+
+  describe(".new", function() {
+    it("delegattes to the client", function() {
+      peep.new(mockSession, PeepData);
+      expect(mockClient.post).toHaveBeenCalled();
+    });
+  });
+});
